405090bce38cf222ac1a7c15e208396145715997
Anas-github-acc <anas.ahamad955@gmail.com>
2024-04-15 17:23:07 +0530

    adding tranversing tree

---
+++ Java/Tree/Tree.java
@@ -0,0 +1,86 @@
+class TreeNode{
+    int data;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+    void setupChild(int left, int right){
+        this.left = new TreeNode(left);
+        this.right = new TreeNode(right);
+    }
+}
+class BinaryTree{
+    TreeNode root;
+    BinaryTree(){
+        this.root = null;
+    }
+    BinaryTree(int data){
+        this.root = new TreeNode(data);
+    }
+    void insert(int data){
+        TreeNode newNode = new TreeNode(data);
+        if(this.root == null){
+            this.root = newNode;
+            return;
+        }
+        TreeNode temp = this.root;
+        while(temp != null){
+            if(temp.left == null){
+                temp.left = newNode;
+                return;
+            }
+            else if(temp.right == null){
+                temp.right = newNode;
+                return;
+            }
+            else{
+                temp = temp.left;
+            }
+        }
+    }
+
+    void printInorder(TreeNode node){
+        if(node == null){
+            return;
+        }
+        printInorder(node.left);
+        System.out.print(node.data + " ");
+        printInorder(node.right);
+    }
+    void printPostorder(TreeNode node){
+        if(node == null){
+            return;
+        }
+        printPostorder(node.left);
+        printPostorder(node.right);
+        System.out.print(node.data + " ");
+    }
+    void printPreorder(TreeNode node){
+        if(node == null){
+            return;
+        }
+        System.out.print(node.data + " ");
+        printPreorder(node.left);
+        printPreorder(node.right);
+    }
+}
+class tree{
+    public static void main(String[] args){
+        BinaryTree tree = new BinaryTree(1);
+        // tree.root.setupChild(2, 3);
+        // tree.root.left.setupChild(4, 5);
+        // tree.root.right.setupChild(6, 7);
+        // tree.root.left.left.setupChild(8, 9);
+        // tree.printInorder(tree.root);
+        // System.out.println();
+        // tree.printPostorder(tree.root);
+        // System.out.println();
+        // tree.printPreorder(tree.root);
+        // System.out.println();
+
+
+    }
+}
